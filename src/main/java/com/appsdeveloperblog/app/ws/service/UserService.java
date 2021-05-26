d19166ad207f6d3f5aa60fbd1723b88806edde96
vpaklatzis <vpaklatzis@gmail.com>
2021-05-26 13:08:19 +0300

    Added Spring Security, encrypted passwords and and made sign-up endpoint public

--- src/main/java/com/appsdeveloperblog/app/ws/service/UserService.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/UserService.java
@@ -1,7 +1,9 @@
 package com.appsdeveloperblog.app.ws.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 	UserDto createUser(UserDto user);
 }
--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -2,6 +2,9 @@ package com.appsdeveloperblog.app.ws.service.impl;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.appsdeveloperblog.app.ws.UserRepository;
@@ -19,6 +22,9 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	Utils utils;
 	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	@Override
 	public UserDto createUser(UserDto user) {
 		
@@ -29,7 +35,7 @@ public class UserServiceImpl implements UserService {
 		
 		String publicUserId = utils.generateUserId(30);
 		userEntity.setUserId(publicUserId);
-		userEntity.setEncryptedPassword("test");
+		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		
 		UserEntity storedUserDetails = userRepository.save(userEntity);
 		
@@ -37,6 +43,12 @@ public class UserServiceImpl implements UserService {
 		BeanUtils.copyProperties(storedUserDetails, returnValue);
 		
 		return returnValue;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
